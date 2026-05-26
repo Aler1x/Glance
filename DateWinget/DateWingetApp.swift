76ad55af3439787bdf983183cee67d3889fc466a
Aler1x <95098796+Aler1x@users.noreply.github.com>
2026-05-26 18:53:57 +0300

    Initial Commit

---
+++ DateWinget/DateWingetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateWingetApp.swift
+//  DateWinget
+//
+//  Created by Alerix on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DateWingetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
